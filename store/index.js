d3a4f917c9bb4c09efd1d2accebea38cb72df935
jackkitte <jackkitte735@gmail.com>
2020-02-09 19:20:16 +0900

    動画情報を取得してストアに保存するまで

--- store/index.js
+++ store/index.js
@@ -2,6 +2,7 @@ import {createRequestClient} from '~/store/request-client';
 
 export const state = () => ({
   items: [],
+  item: {},
   meta: {},
 })
 
@@ -11,12 +12,24 @@ export const actions = {
     const res = await client.get(payload.uri, payload.params)
     commit('mutatePopularVideos', res)
   },
+  async findVideo({commit}, payload) {
+    const client = createRequestClient(this.$axios)
+    const res = await client.get(payload.uri)
+    const params = {
+      ...res.video_list,
+    }
+    commit('mutateVideo', params)
+  },
 }
 
 export const mutations = {
   mutatePopularVideos(state, payload) {
     state.items = payload.items ? state.items.concat(payload.items) : []
     state.meta = payload
+  },
+  mutateVideo(state, payload) {
+    const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
+    state.item = params
   },
 }
 
@@ -26,5 +39,8 @@ export const getters = {
   },
   getMeta(state) {
     return state.meta
-  }
+  },
+  getVideo(state) {
+    return state.item
+  },
 }
