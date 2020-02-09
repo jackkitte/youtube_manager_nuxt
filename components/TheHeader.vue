50d7e10c2b85549896f38858ab20ddb642bbfc6d
jackkitte <jackkitte735@gmail.com>
2020-02-09 18:54:17 +0900

    レイアウトの準備

---
+++ components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <nuxt-link class="navbar-item subtitle is-4" to="/">
+        Youtube Player
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
---
+++ components/TheSidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="column is-2">
+    <div class="section">
+      <aside class="menu">
+        <ul class="mune-list">
+          <li>
+            <nuxt-link to="/">ホーム</nuxt-link>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  </div>
+</template>
